7709056bc2ca63cec2e3241f3487a36992f6ef46
icanas <ivancanas@gmail.com>
2019-02-07 18:12:51 +0100

    Problemas con la traida de datos del servidor asyncrona

--- src/app/dao/dao.service.ts
+++ src/app/dao/dao.service.ts
@@ -10,12 +10,36 @@ export class DaoService {
   constructor(
     private http: HttpClient) { }
 
-    private Url = 'http://localhost:80/api/getCode.php';
 
-    /** GET code de servidor */
-    getCode(): Observable<string> { // Debo definirme tipos de datos que me proporcionen esos valores para recibirlos bien
+    isValid(codigoInsertado: string): boolean {
 
+      const Url = 'http://localhost:80/api/getCode.php';
+      let codigo: string;
+      let valido = false;
+
+      // Aqui le digo que lo que devuelva se llame codigoRecibido y realice lo que esta despues de =>
+      // Con toPromise, mi programa espera la respuesta del servidor, importante!
+      this.http.get<string>(Url).toPromise().then(codigoR => codigo = codigoR);
+
+
+      if (codigo === codigoInsertado) {
+          valido = true;
+      }
+      console.log(valido); // debug
+
+      return valido;
+    }
+
+
+
+    /* GET code de servidor
+    private getCodeSnippet(): Observable<string> { // Debo definirme tipos de datos que me proporcionen esos valores para recibirlos
+      const Url = 'http://localhost:80/api/getCode.php';
       return this.http.get<string>(this.Url);
     }
+
+      this.http.get<string>(Url).subscribe(codigoR => codigo = codigoR); // Me traigo el codigo
+    */
+
 
   }
--- src/app/insertcode/insertcode.component.ts
+++ src/app/insertcode/insertcode.component.ts
@@ -11,22 +11,18 @@ export class InsertcodeComponent implements OnInit {
   constructor(private daoService: DaoService) { }
 
   codigo: string;
+  valido: string;
+
+  validate(): void {
+
+    if (this.daoService.isValid(this.codigo)) {
+      this.valido = 'SI';
+    } else {
+      this.valido = 'NO';
+    }
 
 
-  validate2(): void {
 
-    this.daoService.getCode().subscribe(codigo => this.codigo = codigo);
-
-  }
-
-
-  validate(): boolean {
-
-    let valido = false;
-    valido = false;
-
-
-    return valido;
   }
 
   ngOnInit() {
@@ -34,3 +30,13 @@ export class InsertcodeComponent implements OnInit {
 
 
 }
+
+
+
+/*
+  validate2(): void {
+
+    this.daoService.getCode().subscribe(codigo => this.codigo = codigo);
+
+  }
+*/
