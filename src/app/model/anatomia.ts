efb95b1a5e35cc0eb5afbb58a5f14707c40d8172
icanas <ivancanas@gmail.com>
2019-03-20 15:08:54 +0100

    Mejorado css de medidas

--- src/app/model/anatomia.ts
+++ src/app/model/anatomia.ts
@@ -3,16 +3,51 @@ export class Anatomia {
     email: string;
     peso: number;
     altura: number;
-    cintura: number;
-    brazo: number;
+    PLtriceps: number;
+    PLsubescapular: number;
+    PLbiceps: number;
+    PLcrestaIliaca: number;
+    PLsupraespinal: number;
+    PLabdominal: number;
+    PLmuslo: number;
+    PLpierna: number;
+    PRbrazoRelajado: number;
+    PRbrazoFlexionalo: number;
+    PRcintura: number;
+    PRcadera: number;
+    PRpierna: number;
+    PRmuneca: number;
+    PRtobillo: number;
+    PRantebrazo: number;
+    Dhumero: number;
+    Dfemur: number;
     fechaModificacion: Date;
     activo: string;
 
     constructor() {
+
       this.peso = 0;
       this.altura = 0;
-      this.cintura = 0;
-      this.brazo = 0;
+      this.PLtriceps = 0;
+      this.PLsubescapular = 0;
+      this.PLbiceps = 0;
+      this.PLcrestaIliaca = 0;
+      this.PLsupraespinal = 0;
+      this.PLabdominal = 0;
+      this.PLmuslo = 0;
+      this.PLpierna = 0;
+      this.PRbrazoRelajado = 0;
+      this.PRbrazoFlexionalo = 0;
+      this.PRcintura = 0;
+      this.PRcadera = 0;
+      this.PRpierna = 0;
+      this.PRmuneca = 0;
+      this.PRtobillo = 0;
+      this.PRantebrazo = 0;
+      this.Dhumero = 0;
+      this.Dfemur = 0;
+
+
     }
 
   }
