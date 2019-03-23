546179d5d4c8caa8297f9a966e48d65709142c58
icanas <ivancanas@gmail.com>
2019-03-23 14:00:40 +0100

    Cambio de estrategia, a partir de aqui hago las meidias y las metricas por separado, no compartiran datos

--- src/app/metricas/metricas.component.ts
+++ src/app/metricas/metricas.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DaoService } from '../dao/dao.service';
+
+import { Anatomia } from '../model/anatomia';
+import { Paciente } from '../model/paciente';
+
 @Component({
   selector: 'app-metricas',
   templateUrl: './metricas.component.html',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MetricasComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private daoService: DaoService
+  ) { }
+
+  paciente: Paciente;
+  anatomia: Anatomia = new Anatomia();
+  anatomiaList: Anatomia[] = [];
+
+  IMC: number;
+
+  getAnatomia() {
+
+    this.daoService.getAnatomia(this.paciente).subscribe(
+      R => {
+        if (!R) {
+          this.anatomia = new Anatomia();
+        } else {
+          this.anatomiaList = R;
+          this.anatomia = R[0];
+
+          this.IMC = this.anatomia.peso;
+
+        }
+
+      }
+    );
+
+  }
+
+
 
   ngOnInit() {
+    this.paciente = JSON.parse(localStorage.getItem('Paciente'));
+    this.getAnatomia();
   }
+
 
 }
