552ec5566797dedd819a99f72eba0852fdc2f7e3
icanas <ivancanas@gmail.com>
2019-04-01 15:46:56 +0200

    Añadidos los keyUp, completado el html de metricas y medidas y añadido el calculo de tipo de somatotipo

--- src/app/model/metricas.ts
+++ src/app/model/metricas.ts
@@ -92,12 +92,15 @@ export class Metricas {
 
         ///////////////// Somatotipo /////////////////
 
+        const somatotipoArray: number[] = [];
+
         // Endomorfia
         const sumaPliegues = Number(medidas.PLtriceps) + Number(medidas.PLsubescapular) + Number(medidas.PLsupraespinal);
         const ratio = (170.18) / (Number(medidas.altura) / 100);    // Revisar si metros o cm la altura
         this.Endomorfo = (-0.7182 + (0.1451 * (sumaPliegues * ratio))
                         - 0.00068 * (sumaPliegues * ratio) ** 2)
                         + 0.000004 * ((sumaPliegues * ratio) ** 3);
+        somatotipoArray.push(this.Endomorfo);
 
         // Mesomorfia
         this.Mesomorfo = (
@@ -107,6 +110,7 @@ export class Metricas {
                         + (0.161 * ( Number(medidas.PRpierna) - ( Number(medidas.PLpierna) / 10)))
                         - (0.131 *  Number(medidas.altura))
                         + 4.5);
+        somatotipoArray.push(this.Mesomorfo);
 
         // Ectomorfia
         const IP = Number(medidas.altura) / (Number(medidas.peso) ** (1 / 3));
@@ -120,6 +124,33 @@ export class Metricas {
         } else {
             this.Ectomorfo = 0;
         }
+
+        somatotipoArray.push(this.Ectomorfo);
+
+        // Somatotipo
+        let pos = 0;
+        let indexMax = 0;
+        let max = somatotipoArray[0];
+
+        for (pos; pos < somatotipoArray.length; pos++) {
+            if (somatotipoArray[pos] > max) {
+                indexMax = pos;
+                max = somatotipoArray[pos];
+            }
+          }
+
+        switch (indexMax) {
+            case 0:
+                this.Somatotipo = 'Endomorfo';
+                break;
+            case 0:
+                this.Somatotipo = 'Mesomorfo';
+                break;
+            case 0:
+                this.Somatotipo = 'Ectomorfo';
+                break;
+        }
+
 
     }
 
