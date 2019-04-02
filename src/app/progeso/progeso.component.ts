669b0188013da3d8724eb43ed26563bbcb488d96
icanas <ivancanas@gmail.com>
2019-04-02 17:36:55 +0200

    Agregada grafica de historico corporal

--- src/app/progeso/progeso.component.ts
+++ src/app/progeso/progeso.component.ts
@@ -75,10 +75,10 @@ export class ProgesoComponent implements OnInit {
     ctx = canvas.getContext('2d');
 
     const labels: string[] = [];
-    labels.push('% Grasa');
-    labels.push('% Músculo');
-    labels.push('% Óseo');
-    labels.push('% Residual');
+    labels.push('Grasa %');
+    labels.push('Músculo %');
+    labels.push('Óseo %' );
+    labels.push('Residual %');
 
     // const porcentGrasa = this.metricasList[this.metricasList.length - 1].PorcentGrasa;
     const porcentGrasa = this.metricasList[0].PorcentGrasa;
@@ -114,11 +114,88 @@ export class ProgesoComponent implements OnInit {
 
   }
 
+  chartEvolucionCorporal() {
+
+    let canvas: any;
+    canvas = document.getElementById('EvolucionCorporal');
+    let ctx: any;
+    ctx = canvas.getContext('2d');
+
+    const labels: string[] = [];
+    const datasetGrasa: number[] = [];
+    const datasetMusculo: number[] = [];
+    const datasetOseo: number[] = [];
+    const datasetresidual: number[] = [];
+
+    this.metricasList.forEach(
+      R => {
+        labels.push(R.fechaModificacion.toString().substring(0, 10));
+        datasetGrasa.push(Number(R.PorcentGrasa));
+        datasetMusculo.push(Number(R.PorcentMuscular));
+        datasetOseo.push(Number(R.PorcentOsea));
+        datasetresidual.push(Number(R.PorcentResidual));
+      }
+    );
+
+    const data = {
+      labels,
+      datasets: [{
+          label: 'Grasa %',
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          borderColor: 'rgb(230, 230, 0)',
+          pointBorderColor: 'rgba(255, 0, 0, 1)',
+          pointBackgroundColor: 'rgba(255, 0, 0, 1)',
+          data:  datasetGrasa
+      },
+      {
+          label: 'Musculo %',
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          borderColor: 'rgb(255, 80, 80)',
+          pointBorderColor: 'rgba(255, 0, 0, 1)',
+          pointBackgroundColor: 'rgba(255, 0, 0, 1)',
+          data:  datasetMusculo
+    },
+    {
+          label: 'Oseo %',
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          borderColor: 'rgb(102, 153, 255)',
+          pointBorderColor: 'rgba(255, 0, 0, 1)',
+          pointBackgroundColor: 'rgba(255, 0, 0, 1)',
+          data:  datasetOseo
+  },
+  {
+          label: 'Residual %',
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          borderColor: 'rgb(102, 0, 102)',
+          pointBorderColor: 'rgba(255, 0, 0, 1)',
+          pointBackgroundColor: 'rgba(255, 0, 0, 1)',
+          data:  datasetresidual
+      }]
+    };
+
+    const options = {
+      responsive: true
+    };
+
+    const chart = new Chart(ctx, {
+      // Tipo de grafica
+      type: 'line',
+
+      // Dataset
+      data,
+
+      // Configuracion
+      options
+    });
+
+  }
+
   ngOnInit() {
     this.anatomiaList = JSON.parse(localStorage.getItem('anatomiaList'));
     this.metricasList = JSON.parse(localStorage.getItem('metricasList'));
     this.chartPeso();
     this.chartComposicionCorporal();
+    this.chartEvolucionCorporal();
 
   }
 
